d829016791408cefe4d74079c22688e6e94ad3ca
Drk~Codey <109389152+Drk-codey@users.noreply.github.com>
2023-11-20 14:41:52 +0100

    Add the top-level component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+import Header from "./components/Header";
+import HeroSection from "./components/HeroSection";
+import FeatureOne from "./components/FeatureOne";
+import DailyStats from "./components/DailyStats";
+import { Card } from "./components/Card";
+import insurance1 from "./assets/insurance1.svg";
+import wallet1 from "./assets/wallet1.svg";
+import profit1 from "./assets/insurance1.svg";
+import SocialMedia from "./components/SocialMedia";
+import DevSection from "./components/DevSection";
+import { BlogSection } from "./components/BlogSection";
+import Footer from "./components/Footer";
 
 function App() {
+  const features = [
+    {
+      src: wallet1,
+      title: "Value",
+      description:
+        "Swap from your own wallet. Be your own bank. No centralized bridges or wrapping",
+    },
+    {
+      src: profit1,
+      title: "Yield",
+      description:
+        "Provide Liquidity and Earn Yield on your Native Assets withe Impermanent Loss",
+    },
+    {
+      src: insurance1,
+      title: "Insurance",
+      description: "Get Impermanent Loss Protection on your Liquidity Pools",
+    },
+    {
+      src: wallet1,
+      title: "Staking",
+      description:
+        "Earn Yield on your Native Assets with Impernament Loss Protection",
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="home">
+      <Header />
+      <HeroSection />
+      <DailyStats />
+      <FeatureOne />
+      <div className="features-list-container">
+        {features.map((feature, index) => {
+          return (
+            <Card key={index} title={feature.title} imgUrl={feature.src}>
+              {<p>{feature.description}</p>}
+            </Card>
+          );
+        })}
+      </div>
+      <DevSection />
+      <SocialMedia />
+      <BlogSection />
+      <Footer />
     </div>
   );
 }
